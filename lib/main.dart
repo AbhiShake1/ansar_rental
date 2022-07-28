f157eb4728b9922faaef95f9169088563227826b
abhi <abhi9848038283@gmail.com>
2022-07-28 12:37:52 +0545

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:ansar_rental/app/routes/app_pages.dart';
+import 'package:ansar_rental/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
-import 'app/routes/app_pages.dart';
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-void main() {
   runApp(
     GetMaterialApp(
-      title: "Ansar Rental",
+      title: 'Ansar Rental',
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
